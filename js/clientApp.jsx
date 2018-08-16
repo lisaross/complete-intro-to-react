49366696a47e7b8a68d2dfd5f048c541c90bcc38
Lisa Ross <lr@footprintleadership.com>
2018-08-16 09:16:02 -0400

    :art: clientapp for jsx
    jsx format on same content

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of thrones', color: 'peru' }),
-    ce(MyTitle, { title: 'Stranger things', color: 'orange' }),
-    ce(MyTitle, { title: 'House of Cards', color: 'greenyellow' }),
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of thrones" color="peru" />
+      <MyTitle title="Stranger things" color="orange" />
+      <MyTitle title="House of Cards" color="greenyellow" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
